ab46c817cb1505539e398fd40932c0a2eb08724c
shahidkhanptk <40930850+shahidkhanptk@users.noreply.github.com>
2019-05-08 04:37:05 -0700

    Add files via upload

---
+++ QDemo.java
@@ -0,0 +1,56 @@
+class Queue {
+char q[];
+int putloc, getloc;
+
+queue(int size) {
+q=new char[size];
+putloc=getloc=0;
+}
+
+void put (char ch) {
+if(putloc==q.length) {
+System.out.println(" - Queue is full.");
+return;
+}
+q[putloc++]=ch;
+}
+char get() {
+if(getloc==putloc) {
+System.out.println(" - Queue is empty.");
+return (char) 0;
+}
+return q[getloc++];
+}
+}
+
+
+
+class QDemo {
+public static void main(String args[]) {
+Queue bigQ=new Queue(100);
+Queue smallQ=new Queue(4);
+char ch;
+int i;
+System.out.println("Using bigQ to store the Alphabet.");
+for(i=0;i<26;i++)
+bigQ.put( (char) ('A' + i) );
+Sstem.out.print("Contents of bigQ;");
+for(i=0;i<26;i++) {
+ch=bigQ.get();
+if(ch!=(char) 0) System.out.print(ch);
+}
+System.out.println("\n");
+System.out.println("Using smallQ to generate errors.");
+for(i=0;i<5;i++) {
+System.out.print("Attemping to store" + (char) ('Z' -i));
+smallQ.put((char) ('Z' -i));
+System.put.println();
+}
+System.out.println();
+System.out.print("Contents of smallQ:");
+for(i=0;i<5;i++) {
+ch=smallQ.get();
+if(ch !=(char) 0) System.out.print(ch);
+}
+}
+}
